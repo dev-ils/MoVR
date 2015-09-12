a0ac56b1e54960b26ded6a8f0c0cc5cdbd640d59
Adrian Blanco <adrianblp@gmail.com>
2015-09-12 17:48:32 +0200

    Add listeners for estimote and flic inside Unity

---
+++ Unity/Assets/Scripts/EstimoteListener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Estimote : MonoBehaviour {
+
+	private AndroidJavaObject estimoteData = null;
+	bool hasClicked = false;
+	
+	void Start() {
+		if(estimoteData == null) {
+			
+			using(AndroidJavaClass pluginClass = new AndroidJavaClass("dev_ils.sensordata.FlicData")) {
+				if(pluginClass != null) {
+					estimoteData = pluginClass.CallStatic<AndroidJavaObject>("instance");
+					estimoteData.Call<bool>("hasClicked");
+				}
+			}
+		}
+	}
+}
