580c408284fa52ab1630f5373b9e66c7943ea986
Adrian Blanco <adrianblp@gmail.com>
2015-09-13 02:18:47 +0200

    Try to make android linking work

---
+++ Unity/Assets/Scripts/AndroidListener.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AndroidListener : MonoBehaviour {
+
+	public Text textHolder;
+	private string statusText = "Failure!";
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID
+
+		AndroidJavaClass ajc = new AndroidJavaClass("com.lokisystems.oshook.Bridge");
+		string ret = ajc.CallStatic<string>("ReturnString");
+
+		Debug.Log("Android: returned " + ret);
+
+		if (ret == null){
+			statusText = "Ret null";
+		} else if (ajc == null) {
+			statusText = "Class null";
+		} else {
+			statusText = ret;
+		}
+
+		#endif
+	}
+	
+	// Update is called once per frame
+	void onGui () {
+		textHolder.text = statusText;
+	}
+}
